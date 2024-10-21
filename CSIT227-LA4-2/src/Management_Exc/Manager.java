4742a9a1bf1a9b9d5f4041b625bb8cb2ce067b61
IgnisFrostburn <159628739+IgnisFrostburn@users.noreply.github.com>
2024-10-21 09:51:20 +0800

    Add files via upload

---
+++ CSIT227-LA4-2/src/Management_Exc/Manager.java
@@ -0,0 +1,23 @@
+package Management_Exc;
+
+public class Manager extends Employee {
+    public Manager(String name, int age, double salary) {
+        super(name, age, salary);
+    }
+
+    /**
+     * TODO this implementation
+     * @param e the employee to be given raise
+     * @param raise the raise
+     * @throws IllegalArgumentException when raise is negative
+     */
+    public void giveRaise(Employee e, double raise) {
+        e.setSalary(raise);
+        setSalary(raise/2);
+    }
+
+    @Override
+    public String toString() {
+        return "Mgr. " +super.toString();
+    }
+}
